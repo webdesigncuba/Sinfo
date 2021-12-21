ecd3f00a01efdc74813647e456ab78f563787651
DavidyBertha <webdesigncuba@gmail.com>
2021-12-21 00:50:28 -0500

    Agregado Fontawesome y Static

--- sinfo/sinfo/urls.py
+++ sinfo/sinfo/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from index import views
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('marca/', include('config.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
